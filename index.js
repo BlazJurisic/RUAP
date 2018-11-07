c3d3a588e4b51f99f2ad876b0d069ac66a275671
BlazJurisic <jurisic.blaz@gmail.com>
2018-11-07 15:38:32 +0100

    cherry picked commit, fixed conflict, index.js.orig kept

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-console.log("Pozdrav iz aplikacije od Blaza Jurisica, index mi je R3785, vald"); //gramaticki je neispravno jer ispada da je app napravljen od materijala blaz jurisic
+console.log("Pozdrav iz aplikacije od Blaza Jurisica"); //gramaticki je neispravno jer ispada da je app napravljen od materijala blaz jurisic
